af47b1f1eff2e85bc3854b43a3e6b30223c367fe
yakushiji <yaoshisizhenghe@gmail.com>
2013-10-08 20:30:54 +0900

    Set haml and sass

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,7 @@ NiceCm::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  config.sass.preferred_syntax = :sass
+  config.sass.syntax = :sass
 end
