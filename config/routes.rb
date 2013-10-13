b8de5a1cbe41563308927ac23b0df776ee208c9c
yakushiji <yaoshisizhenghe@gmail.com>
2013-10-14 01:58:14 +0900

    cm-controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 NiceCm::Application.routes.draw do
+  root :to => 'cms#index'
+  get "cms/index"
+  get "cms/show"
+  get "cms/new"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -46,9 +51,6 @@ NiceCm::Application.routes.draw do
   #     resources :products
   #   end
 
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
